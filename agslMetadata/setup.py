c773e8689981c40b5828c0b27bd5e9074dce0f41
Stephen Appel <srappel930@gmail.com>
2023-10-09 12:51:15 -0500

    create package for pip

---
+++ agslMetadata/setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup, find_packages 
+
+setup(name='agslMetadata', 
+      version='1.0', 
+
+description='agslMetadata', 
+      author='Stephen Appel', 
+      author_email='srappel@uwm.edu', 
+      license='Apache2.0', 
+      packages=find_packages(),  
+      zip_safe=False) 
