830cd766cecb943819004c232538c97163afe81e
Stephen Appel <srappel930@gmail.com>
2023-10-24 11:04:18 -0500

    update

--- geometadataedit/geometadataedit/batchingest.py
+++ geometadataedit/geometadataedit/batchingest.py
@@ -8,10 +8,10 @@ load_dotenv()
 
 warnings = []
 
-CSV_OUTPUT = Path(os.getenv("LOG_PATH"))
+CSV_OUTPUT = Path(r'S:\GeoBlacklight\project-files\fileProcessing\logs\LOG20231023.csv')
 
 # Take as an argument a directory containing a bunch of datasets (in dirs)
-target_directory = Path(r"S:\_R_GML_Archival_AGSL\GIS_Data\GeoBlacklight\public")
+target_directory = Path(r"S:\GeoBlacklight\project-files\fileProcessing\ready_to_process")
 
 ### Add it to a CSV file so we have a log of all these!
 csvfile = open(CSV_OUTPUT, 'w', newline='')
@@ -67,14 +67,8 @@ def main():
         status = "passing"
         if dataset_directory[1] == 1: # Only children of root
 
-    # Create a updateMetadata.Dataset object by passing each dir Path
-    # to the Dataset factory Dataset(Path)
-    # This will create:
-    #   - Dataset.data (Path to the actual data)
-    #   - Dataset.datatype (Integer code)
-    #   - Dataset.metadata (A AGSLMetadata Object)
             try:
-                dataset = updateMetadata.Dataset(Path(dataset_directory[0]))
+                dataset = updatemetadata.Dataset(Path(dataset_directory[0]))
             except Exception as error:
                 warning = f"Failed to create Dataset object for {str(dataset_directory[0])}\n"
                 print(warning)
@@ -82,24 +76,25 @@ def main():
                 warnings.append(warning)
                 warnings.append(error)
                 status = "failing"
-                logwriter.writerow([dataset_directory[0], status, dataset.metadata.identifier.assignedName, warning, error])
+                logwriter.writerow([dataset_directory[0], status, "none assigned", warning, error])
                 purge()
                 continue # Will go to the next Dataset.
 
-    # Creating the Dataset object will create the AGSLMetadata object:
-    # This creates:
-    #   - Dataset.metadata.xml_text
-    #   - Dataset.metadata.md_object
-    #   - Dataset.metadata.rootElement
-    #   - Dataset.metadata.altTitle
-    #   - Dataset.metadata.rights
-
-    ### Now we need to run through some functions on the data:
-
-    ## Create and Write Identifiers:
-    ## Dataset.metadata.create_and_write_identifiers()
             try:
-                dataset.metadata.create_and_write_identifiers()
+                existing_identifier = dataset.metadata.get_existing_identifier_or_mint()
+            except Exception as error:
+                warning = f"Failed to create existing_identifier object for {str(dataset_directory[0])}\n"
+                print(warning)
+                print(error)
+                warnings.append(warning)
+                warnings.append(error)
+                status = "failing"
+                logwriter.writerow([dataset_directory[0], status, "none assigned", warning, error])
+                purge()
+                continue # Will go to the next Dataset.
+
+            try:
+                dataset.metadata.write_identifiers(existing_identifier)
             except Exception as error:
                 warning = f"Failed to create and write identifiers for {str(dataset_directory[0])}\n"
                 print(warning)
@@ -110,19 +105,7 @@ def main():
                 logwriter.writerow([dataset_directory[0], status, dataset.metadata.identifier.assignedName, warning, error])
                 purge()
                 continue
-    # First, this will mint a new Identifier object.
-    # This creates:
-    #   -Dataset.metadata.identifier.arkid
-    #   -Dataset.metadata.identifier.nameAuthorityNumber
-    #   -Dataset.metadata.identifier.assignedName
 
-    # Next, it will write the infomration from the Identifier object
-    # into the correct places in the xml metadata.
-
-    # Finally, it saves the metadata object.
-
-    ## Update AGSL Hours
-    ## Dataset.metadata.update_agsl_hours() 
             try:
                 dataset.metadata.update_agsl_hours()
             except Exception as error:
@@ -135,10 +118,7 @@ def main():
                 logwriter.writerow([dataset_directory[0], status, dataset.metadata.identifier.assignedName, warning, error])
                 purge()
                 continue
-    # Updates the hours in the metadata and saves the metadata object.
 
-    ## Dual Metadata Export
-    ## Dataset.metadata.dual_metadata_export
             try:
                 dataset.metadata.dual_metadata_export()
             except Exception as error:
@@ -151,10 +131,7 @@ def main():
                 logwriter.writerow([dataset_directory[0], status, dataset.metadata.identifier.assignedName, warning, error])
                 purge()
                 continue
-    # This exports the metadata to the parent directory for the dataset
 
-    ## Bind the ArkID using NOID
-    ## Dataset.metadata.bind()
             try:
                 dataset.metadata.bind()
             except Exception as error:
@@ -167,16 +144,7 @@ def main():
                 logwriter.writerow([dataset_directory[0], status, dataset.metadata.identifier.assignedName, warning, error])
                 purge()
                 continue
-    # Creates the bind parameters:
-    # who, what, when, where, meta-who, meta-when, meta-uri, rights, download
-    # Sends the bind request using the requests library
 
-    ### Now we need to "ingest" the dataset, which involves
-    # Creating a new zip archive containing the data and xml metadata
-    # The zip file will be named after the alt-title
-    # Put it in a directory named after the assignedName from the arkid
-    # Put that directory in the appropriate folder on the webserver (by rights)
-    # Create a copy of the ISO Xml (assignedName_ISO.xml) in the metadata directory on the webserver
             try:
                 dataset.ingest()
             except Exception as error:
--- geometadataedit/geometadataedit/movedatasets.py
+++ geometadataedit/geometadataedit/movedatasets.py
@@ -5,11 +5,12 @@ Move datasets according to a csv of dataset directory paths
 from pathlib import Path
 import csv
 
-movecsv_path = Path(r"C:\Users\srappel\Desktop\move.csv")
+movecsv_path = Path(r"C:\Users\srappel\Desktop\redo.csv")
+destpath = Path(r"S:\GeoBlacklight\project-files\fileProcessing\failed_metadata_processing")
 movecsv = open(movecsv_path, "r")
 reader = csv.reader(movecsv)
 
 for row in reader:
     dirpath = Path(row[0])
-    destpath = dirpath.parent.parent / "redo" / dirpath.stem
-    dirpath.rename(destpath)
+    output = destpath / dirpath.stem
+    dirpath.rename(output)
