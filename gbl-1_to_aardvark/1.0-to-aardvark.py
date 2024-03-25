9e3634931662132390cc600c958fa054a9ccd90f
Stephen Appel <srappel930@gmail.com>
2024-03-25 13:03:12 -0500

    uwm localization

--- gbl-1_to_aardvark/1.0-to-aardvark.py
+++ gbl-1_to_aardvark/1.0-to-aardvark.py
@@ -2,15 +2,23 @@ import json
 import csv
 import os
 
+from datetime import datetime
+from pathlib import Path
+
 # Manual changes before run
-dir_crosswalk = 'crosswalk.csv'
+
+# Path to crosswalk definition
+dir_crosswalk = Path('crosswalk.csv')
 # add directory of JSON files in the 1.0 schema
-dir_old_schema = '1.0/'
+dir_old_schema = Path(r"S:\GeoBlacklight\project-files\opengeometadata_GBL-1\opengeometadata\edu.princeton")
 #add directory for new JSON files in the Aardvark schema
-dir_new_schema = 'aardvark/'
+dir_new_schema = Path('aardvark/princeton/')
+
+#Default values
+RESOURCE_CLASS_DEFAULT = "Maps"
+PLACE_DEFAULT = ""
 
 # Load the crosswalk.csv and make it a dictionary
-
 crosswalk = {}
 with open(dir_crosswalk, encoding='utf8') as f:
     reader = csv.reader(f)
@@ -20,6 +28,66 @@ with open(dir_crosswalk, encoding='utf8') as f:
         new = record[1]
         crosswalk[old] = new
 
+# Function to convert fields that ends with '_sm' to an array
+# SRA - Moved this to before the other function because it calls this function
+def string2array(data_dict):
+    for key in data_dict.keys():
+        suffix = key.split('_')[-1]
+        if suffix == 'sm' or suffix == 'im':
+            val = data_dict[key]
+            if type(val) != list:
+                data_dict[key] = [val]
+    return data_dict
+
+def check_required(data_dict):
+    # Check if required fields are present
+    requirements = ["dct_publisher_sm", "dct_spatial_sm", "gbl_mdVersion_s", "dct_title_s", "gbl_resourceClass_sm", "id", "gbl_mdModified_dt"]
+    
+    for req in requirements:
+        if req not in data_dict:
+            print (f"Requirement {req} is not present...")
+            
+            if req == "gbl_resourceClass_sm":
+                if "dc_type_s" not in data_dict:
+                    print(f"No dc_type_s information found.")
+                    data_dict["gbl_resourceClass_sm"] = [RESOURCE_CLASS_DEFAULT]
+                elif data_dict["dc_type_s"] == "Dataset":
+                    data_dict["gbl_resourceClass_sm"] = ["Datasets"]
+                    print(f"Replaced dc_type_s:Dataset with gbl_resourceClass_sm:Datasets")
+                elif data_dict["dc_type_s"] == "Image":
+                    data_dict["gbl_resourceClass_sm"] = ["Imagery"]
+                    print(f"Replaced dc_type_s:Dataset with gbl_resourceClass_sm:Imagery")
+                else:
+                    data_dict["gbl_resourceClass_sm"] = [RESOURCE_CLASS_DEFAULT] # change if needed!
+                    print(f"Replaced dc_type_s:Null with gbl_resourceClass_sm:{RESOURCE_CLASS_DEFAULT}")
+                    
+            elif req == "dct_spatial_sm":
+                data_dict["dct_spatial_sm"] = [PLACE_DEFAULT]
+                print(f"Replaced dct_spatial_sm:Null with dct_spatial_sm:{PLACE_DEFAULT}")
+                
+            elif req == "gbl_mdModified_dt":
+                data_dict["gbl_mdModified_dt"] = datetime.utcnow().strftime("%Y-%m-%dT%H:%M:%SZ")
+                
+            elif req == "dct_publisher_sm":
+                print(f"No Publisher information found.")
+                if "dct_creator_sm" not in data_dict:
+                    print(f"No Creator information found.")
+                elif data_dict["dct_creator_sm"] != "":
+                    data_dict["dct_publisher_sm"] = data_dict["dct_creator_sm"]
+                    print(f"Replaced dct_publisher_sm:Null with dct_publisher_sm:{data_dict['dct_creator_sm']}")
+                else:
+                    continue
+    return
+
+def remove_deprecated(data_dict):
+    # Remove the deprecated fields from the output Aardvark
+    deprecated = ["dc_type_s", "layer_geom_type_s", "dct_isPartOf_sm", "uw_supplemental_s", "uw_notice_s", "uuid"]
+    for field in deprecated:
+        if field in data_dict:
+            data_dict.pop(field)
+            print(f"Removed the deprecated {field} field.")
+    
+    return
 
 # Function to update the metadata schema
 def schema_update(filepath):
@@ -28,36 +96,68 @@ def schema_update(filepath):
         # Load its content and make a new dictionary
         data = json.load(fr)
 
+        if data.__class__ != dict:
+            return
+
         # Loop over crosswalk to change dictionary keys
         for old_schema, new_schema in crosswalk.items():
             if old_schema in data:
                 data[new_schema] = data.pop(old_schema)
-            
+        
+        # Change the metadata type:
+        data["gbl_mdVersion_s"] = "Aardvark"
+
+        # Remove geoblacklight_version
+        if "geoblacklight_version" in data:
+            data.pop("geoblacklight_version")
+        
+        # Check for required fields:
+        check_required(data)
+        
+        # Remove deprecated fields
+        remove_deprecated(data)
+        
     # check for multi-valued fields - if so, convert its value to an array
     data = string2array(data)
 
     # Write updated JSON to a new folder
-    filepath_updated = dir_new_schema + file
-    with open(filepath_updated, 'w', encoding='utf8') as fw:
+    if filepath.name != "geoblacklight.json":
+        filepath_updated = dir_new_schema / filepath.name
+    else:
+        id_filepath = Path(f"{data['id']}.json")
+        filepath_updated = dir_new_schema / id_filepath
+
+    with open(filepath_updated, 'w') as fw:
         j = json.dumps(data, indent=2)
         fw.write(j)
-
-# Function to convert fields that ends with '_sm' to an array
-def string2array(dict):
-    for key in dict.keys():
-        suffix = key.split('_')[-1]
-        if suffix == 'sm' or suffix == 'im':
-            val = dict[key]
-            if type(val) != list:
-                dict[key] = [val]
-    return dict
 
 
 # Collect all JSON files in a list
 # Iterate the list to update metadata schema
-files = [x for x in os.listdir(dir_old_schema) if x.endswith('.json')]
-for file in files:
-    print(f'Executing {file} ...')
-    filepath = dir_old_schema + file
-    schema_update(filepath)
+def list_all_json(rootdir): # -> list[pathlib.Path]
+    '''List All .json files in a given dir'''
 
+    rootdir = Path(rootdir)
+    files = []
+    for path in sorted(rootdir.rglob("*.json")):
+        # skip layers.json
+        if path.name == 'layers.json':
+            print(f"Skipped {path.name}")
+            continue
+        else:
+            files.append(Path(path))
+
+    return files
+
+# Main function:
+def main_function():
+    if not dir_new_schema.exists():
+        dir_new_schema.mkdir()
+
+    files = list_all_json(dir_old_schema)
+    for file in files:
+        print(f'Executing {file} ...')
+        schema_update(file)
+
+if __name__ == '__main__':
+    main_function()
--- gbl-1_to_aardvark/readme.md
+++ gbl-1_to_aardvark/readme.md
@@ -1,21 +1,51 @@
+# UW-Milwaukee AGSL
 # GeoBlacklight Metadata 1.0 to OpenGeoMetadata version Aardvark
 
-This script will convert the crosswalkable fields in a batch of JSON files in the [GeoBlacklight 1.0 schema](https://opengeometadata.org/docs/gbl-1.0) into the [OpenGeoMetadata Aardvark schema](https://opengeometadata.org/docs/ogm-aardvark)
+## Last modified:
+3/14/2024 by SRA (See [UWM Changelog](#UWM-Changelog))
+
+This script will convert the crosswalkable fields in a batch of JSON files in the
+[GeoBlacklight 1.0 schema](https://opengeometadata.org/docs/gbl-1.0)
+into the [OpenGeoMetadata Aardvark schema](https://opengeometadata.org/docs/ogm-aardvark)
 
 A set of sample JSONs in the 1.0 schema is included for testing.
 
 ## How it works
-The script will open the existing JSONs, scan the field names (keys), use the crosswalk.csv file to convert the names, and write new JSONs with the new field names. If will also update the field type from single to array if needed.
 
-If a field name is present in the existing JSONs, but not in the crosswalk, it will include them unchanged in the new JSONS.
+The script will open the existing JSONs, scan the field names (keys), use the crosswalk.csv file
+to convert the names, and write new JSONs with the new field names. If will also update the field
+type from single to array if needed.
+
+If a field name is present in the existing JSONs,
+but not in the crosswalk, it will include them unchanged in the new JSONs.
 
 ## Limitations
-This script only changes the field name and type for elements that can be directly crosswalked. Any fields that are not listed in crosswalk.csv will be carried over to the new Aardvark JSONS. This prevents data loss, but means that the new documents will still include deprecated fields.
 
+This script only changes the field name and type for elements that can be directly crosswalked.
+Any fields that are not listed in crosswalk.csv will be carried over to the new Aardvark JSONS.
+This prevents data loss, but means that the new documents ~~will still include deprecated fields~~
+include local fields from other institutions that aren't removed in the remove_deprecated() function.
 
 ## How to run
+
 1. Add 1.0 metadata JSON documents to a folder in the directory
 2. Open the 1.0-to-aardvark.py script in a text editor
 3. Change the value for dir_old_schema to the name of your folder holding the JSON documents
 4. Run the script from within the editor or using the command line
 
+## UWM Changelog:
+
+1. [Add support for Pathlib](https://github.com/UWM-Libraries/gbl-1_to_aardvark/commit/2ba511a26b4730c5c77fab7e937b02a3201d8d75)
+to allow more robust directory structure 
+2. Adds new functions for dealing with common metadata issues and adds those functions as subfunctions of `schema_update()`:
+    - `check_required()` checks for the presence of required fields (defined in an array) and warns if missing and
+    tries to make logical replacements or fill with default values defined at the top of the script
+    - `remove_deprecated()` will remove deprecated fields (defined in an array)
+3. Adds an output folder (./aardvark) with a .gitkeep file and ignores contents of that folder to avoid tracking output.
+(Part of [PR](https://github.com/OpenGeoMetadata/gbl-1_to_aardvark/pull/4))
+4. Force the `gbl_mdVersion_s` field to 'Aardvark' instead of crosswalking from the deprecated `geoblacklight_version` field.
+(Part of [PR](https://github.com/OpenGeoMetadata/gbl-1_to_aardvark/pull/4))
+5. Removes `geoblacklight_version,gbl_mdVersion_s` from the crosswalk.
+(Part of [PR](https://github.com/OpenGeoMetadata/gbl-1_to_aardvark/pull/4))
+6. [Adds recursive searching](https://github.com/UWM-Libraries/gbl-1_to_aardvark/commit/4c80f4585e8855d175faf7f9208bf6bfa35e953a)
+using pathlib and glob.
