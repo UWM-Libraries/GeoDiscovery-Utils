2fbbb24b34df41ae0d66ab1c4b6dc1d476e10457
Stephen Appel <srappel930@gmail.com>
2024-03-28 13:35:06 -0500

    some fixes

--- arkchecker/arkchecker.py
+++ arkchecker/arkchecker.py
@@ -1,6 +1,5 @@
 # This python file will loop through a directory of OGM Aardvark metadata
 # and check with NOID if the Ark ID is properly bound and redirected.
-
 import json
 import requests
 import re
@@ -8,16 +7,18 @@ import re
 from pathlib import Path
 
 # Constants
-AARDVARK_DIR = "uwm_fixture/Aardvark"
+AARDVARK_DIR = r"C:\Users\srappel\Documents\GitHub\GeoDiscovery-Utils\uwm_fixture\Aardvark"
 NOID_PROD = r"https://digilib-admin.uwm.edu/noidu_gmgs"
 
 # Assertions
-# assert requests.get(NOID_PROD).status_code == 200
+# pip install pip_system_certs if receiving a SSL error.
+assert requests.get(NOID_PROD).status_code == 200
 assert Path(AARDVARK_DIR).is_dir()
 
 
 def searchForID(text) -> list:
     # Will match the whole ID
+    # Group 0 will be the whole string
     # Group 1 will be the *Name Assigning Authority Number*
     # Group 2 will be the *Assigned Name*
     arkregex = re.compile(r"(\d{5})\/(\w{11})")
@@ -27,6 +28,8 @@ def searchForID(text) -> list:
 
 def listMetadata(dir) -> list[str]:
     dir = Path(dir)  # ensure is Path object
+    assert dir.is_dir
+
     mdlist = []
     for mdfilePath in dir.rglob("*.json"):
         with open(mdfilePath, "r", encoding="utf8") as mdfile:
@@ -53,19 +56,19 @@ def NOIDfetch(arkid) -> str:
 
 
 # Until Karl is able to get a cert for the digitlib-admin domain, we can use this return text for testing:
-fetch_result = """
-id:    77981/gmgs0c4sj3x hold
-Circ:  i|20230925154243|@10.62.92.248 apache/apache|157
-download: https://geodata.uwm.edu/public/gmgs0c4sj3x/ClarkCounty_CensusBlocks_2002.zip
-meta-uri: https://geodata.uwm.edu/metadata/gmgs0c4sj3x_ISO.xml
-meta-when: 2023-09-25T15:42:43
-meta-who: University of Wisconsin-Milwaukee Libraries
-rights: public
-what: Census Blocks Clark County, Wisconsin 2002
-when: 2002-01-01T00:00:00
-where: https://geodiscovery.uwm.edu/catalog/ark:-77981-gmgs0c4sj3x
-who: Legislative Technology Services Bureau
-"""
+# fetch_result = """
+# id:    77981/gmgs0c4sj3x hold
+# Circ:  i|20230925154243|@10.62.92.248 apache/apache|157
+# download: https://geodata.uwm.edu/public/gmgs0c4sj3x/ClarkCounty_CensusBlocks_2002.zip
+# meta-uri: https://geodata.uwm.edu/metadata/gmgs0c4sj3x_ISO.xml
+# meta-when: 2023-09-25T15:42:43
+# meta-who: University of Wisconsin-Milwaukee Libraries
+# rights: public
+# what: Census Blocks Clark County, Wisconsin 2002
+# when: 2002-01-01T00:00:00
+# where: https://geodiscovery.uwm.edu/catalog/ark:-77981-gmgs0c4sj3x
+# who: Legislative Technology Services Bureau
+# """
 
 
 def whereEditor(arkid) -> str:
@@ -74,16 +77,20 @@ def whereEditor(arkid) -> str:
         NOID_PROD
         + f"?bind+set+{arkid[0]}+where+https://geodiscovery.uwm.edu/catalog/ark:-{arkid[1]}-{arkid[2]}"
     )
-    # edit_r = requests.get(editURL)
+    edit_r = requests.get(editURL)
 
     # TODO: Handle a non 200 status code
-    # assert edit_r.status_code == 200
+    assert edit_r.status_code == 200
 
     return editURL
 
 
 if __name__ == "__main__":
-    print(listMetadata(AARDVARK_DIR))
-    arkid = searchForID(fetch_result)
-    editURL = whereEditor(arkid)
-    print(editURL)
+    assert Path(AARDVARK_DIR).is_dir
+
+    for md_id in listMetadata(AARDVARK_DIR):
+        # Fetch the ID
+        arkid = searchForID(NOIDfetch(md_id))
+        editURL = whereEditor(arkid)
+        print(editURL)
+        print("-----------------------")
